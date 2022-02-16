482dbd3d7ea823f85fea4c2d9d44c1671fb5796c
Arshpreet33 <arshpreetsingh33@gmail.com>
2022-02-16 17:12:25 -0500

    Added Single Player functionality for random computer values

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ $('document').ready(function () {
     var turn = 'X'
     var counter = 0
     var gameFinished
+    var multiplayer = 1
+    var emptyMatrixValues = ['00', '01', '02', '10', '11', '12', '20', '21', '22']
 
     function checkMatchWin(player, x, y) {
         if (matrix[x][0] == player && matrix[x][1] == player && matrix[x][2] == player) {
@@ -30,6 +32,7 @@ $('document').ready(function () {
 
         document.getElementById('' + x + '' + y).innerHTML = turn
         matrix[x][y] = turn
+        emptyMatrixValues.splice(emptyMatrixValues.indexOf(x + '' + y), 1)
 
         var p = document.getElementsByTagName('p')[0]
         if (checkMatchWin(turn, x, y)) {
@@ -46,19 +49,64 @@ $('document').ready(function () {
             gameFinished = 1
             return
         }
+
+        if (!multiplayer && turn == '0') {
+            computerTurn()
+        }
     }
 
     $('#reset').click(function () {
+        reset()
+    })
+
+    function reset() {
+
         matrix = [[null, null, null], [null, null, null], [null, null, null]]
-        $('.boxes').text('')
         gameFinished = null
         counter = 0
         turn = 'X'
+        emptyMatrixValues = ['00', '01', '02', '10', '11', '12', '20', '21', '22']
+        $('.boxes').text('')
         document.getElementsByTagName('p')[0].innerHTML = 'Game starts with X..!!! Winner will be shown here...'
-    })
+    }
 
     $('.boxes').click(function () {
         insertValue(this.id[0], this.id[1])
     })
+
+    $('input[type=radio][name=GameType]').change(function () {
+        if (this.value == 'SinglePlayer') {
+
+            multiplayer = null
+            reset()
+        }
+        else if (this.value == 'MultiPlayer') {
+            multiplayer = 1
+            reset()
+        }
+    });
+
+    //Single Player functions
+    function randomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function getAvailableCells() {
+        var emptyMatrixValues = []
+
+        matrix.forEach(x => {
+            matrix.forEach(y => {
+                if (!x && !y) emptyMatrixValues.push(x + '' + y)
+            });
+        });
+        return emptyMatrixValues
+    }
+
+    function computerTurn() {
+        var computerChoose = Math.random() * emptyMatrixValues.length
+        insertValue(emptyMatrixValues[parseInt(computerChoose)][0], emptyMatrixValues[parseInt(computerChoose)][1])
+    }
+
+
 
 })
